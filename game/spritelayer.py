4999561c34a808286775aab96382d3e067f900ff
Ben <benalbreck@gmail.com>
2014-10-21 08:34:37 -0400

    SpriteLayer only updates object lists (actually a tuple, self.children) when changes are made to it instead of every frame.
    Added lots of comments to SpriteLayer and luca.py

--- game/spritelayer.py
+++ game/spritelayer.py
@@ -19,23 +19,29 @@ class SpriteLayer( cocos.layer.Layer ):
         Initialize some sprites on the screen and call the
         schedule function
         """
+        # Internal Clock of SpriteLayer
         self.time = 0
 
         # Superclass constructor
         super(SpriteLayer, self ).__init__(*args, **kwargs)
 
-        x, y = director.get_window_size()
-        self.x_max = x
-        self.y_max = y
+        # Get coordinates of upper right corner (max size)
+        self.x_max, self.y_max = director.get_window_size()
         print "Game Dimensions: ", self.x_max,"x", self.y_max
 
+        # Load objects into the game - spawns cells, food, etc.
         self.load()
 
-        # Call update function every frame
+        # Set up scheduler to call update function every frame
         self.schedule(self.update)
 
 
     def load(self):
+        """
+        Initial load method for start of game
+        Eventually this will call a general spawn function
+        self.spawn(10, Type = 'cell')
+        """
         self.add(cell.Cell(position=(300,300), color=colors.blue))
         self.add(cell.Cell(position=(300,300), color=colors.blue))
         self.add(cell.Cell(position=(300,300), color=colors.blue))
@@ -46,6 +52,7 @@ class SpriteLayer( cocos.layer.Layer ):
         self.add(food.Food(position=(random.randint(250,350), random.randint(250,350))))
         #self.add(food.Food(position=(random.randint(0, x), random.randint(0,y))))
         """
+        Example of self.spawn:
         while self.counter[self.indices[Type]] < Num:
             new_obj = self.types[Type](box=self.box,
                                 name=Type+str(self.counter[self.indices[Type]]),
@@ -73,10 +80,8 @@ class SpriteLayer( cocos.layer.Layer ):
         So to call first object's update member function we call
             self.children[0][1].update(dt)
         """
+        # There has got to be some internal clock I can use...
         self.time += 1
-        # Spawn food every 120 frames for now
-        if self.time % 120 == 0:
-            self.add(food.Food(position=(random.randint(0,self.x_max), random.randint(0,self.y_max))))
 
         # Check collisions from last dt
         self.check_collisions()
@@ -88,8 +93,11 @@ class SpriteLayer( cocos.layer.Layer ):
         to_add = self.update_objects(dt, to_add)
 
         # Remove objects for this dt
-        to_add = self.remove_objects(to_add)
+        to_add = self.remove_dead(to_add)
 
+        # Spawn food every 120 frames for now
+        if self.time % 120 == 0:
+            self.add(food.Food(position=(random.randint(0,self.x_max), random.randint(0,self.y_max))))
         # Spawn matter if necessary
         # to_add = self.spawn_matter(to_add)
 
@@ -100,6 +108,11 @@ class SpriteLayer( cocos.layer.Layer ):
 
 
     def check_collisions(self):
+        """
+        Check collisions between cells and anything else
+        Objects interact if they collide and are not dead
+        Should try out cocos2d collision checker in the future
+        """
         for i in xrange(len(self.children)):
             if self.children[i][1].Type == 'cell':
                 for j in xrange(1, len(self.children)):
@@ -113,17 +126,43 @@ class SpriteLayer( cocos.layer.Layer ):
 
 
     def update_objects(self, dt, to_add):
+        """
+        Simple loop to update all objects in game
+        """
         for obj in ( i[1] for i in self.children):
-            if obj.Type == 'cell':
-                obj.get_objlist(self.children)
             obj.update(dt)
 
 
-    def remove_objects(self, to_add):
+    def remove_dead(self, to_add):
+        """
+        Remove objects that are dead
+        """
         for to_remove in (i[1] for i in self.children if i[1].dead):
-             #to_add.extend(to_remove.new_obj)
-             self.remove(to_remove)
-             to_remove.delete()
+            # This adds anything the dead object leaves behind before it is removed
+            #to_add.extend(to_remove.new_obj)
+            # This takes objects off self.children tuple, and updates objects' tuples
+            self.remove(to_remove)
+            # This calls the dead object's deconstructor, and it ceases to exist from this point on
+            to_remove.delete()
         return to_add
 
 
+    def remove(self, child):
+        """
+        Update children list in each relevant object as well
+        """
+        super(SpriteLayer, self).remove(child)
+        for obj in ( i[1] for i in self.children):
+            # TODO: Check an object Boolean variable instead of Type
+            if obj.Type == 'cell':
+                obj.get_objlist(self.children)
+
+    def add(self, child):
+        """
+        Update children list in each relevant object as well
+        """
+        super(SpriteLayer, self).add(child)
+        for obj in ( i[1] for i in self.children):
+            if obj.Type == 'cell':
+                obj.get_objlist(self.children)
+        
--- luca.py
+++ luca.py
@@ -4,45 +4,40 @@ from game.inputs import keydisplay, mousedisplay
 from game import menu
 from game.colors import *
 
-
 if __name__ == "__main__":
-    # Instantiate main director
+    """
+    Main function of game
+    """
+    # Instantiate and Initialize main director
     director = cocos.director.director
-
-    # Initialize main cocos director (window)
     director.init(resizable=True)
+
+    # Instantiate main scene
     scene = cocos.scene.Scene()
 
-
-    # Run a scene with our event displayers:
+    # Instantiate sprite layer - layer with all the sprite objects
     spritelayer = spritelayer.SpriteLayer()
+
+    # Instantiate background layer - just a colored layer
     backgroundlayer = cocos.layer.ColorLayer(base3[0], base3[1], base3[2], 255)
 
+    # Instantiate main menu layer - main menu at start of game
+    mainmenulayer = menu.MainMenu()
+
+    # Add our layers in a multiplex (only 1 layer per multiplex can be visible at a time)
+    # The default visible layer is the first in the multiplex (mainmenu)
+    # This multiplex layer is indexed z = 1
     scene.add( cocos.layer.base_layers.MultiplexLayer(
-                                                    menu.MainMenu(),
+                                                    mainmenulayer,
                                                     spritelayer
                                                     ),
                                                  z=1)
+    # Add background, always visible, indexed at z = 0
     scene.add( backgroundlayer, z=0)
 
+    # DEBUG - shows what key is pressed and mouse coords
+    # scene.add( mousedisplay.MouseDisplay(), z = 2)
+    # scene.add( keydisplay.KeyDisplay(), z = 3)
+
+    # Tell director to run our scene (start game!)
     director.run( scene )
-
-    # Sample of running multiple scenes at once:
-    #cocos.director.director.run( cocos.scene.Scene( keydisplay.KeyDisplay(), mousedisplay.MouseDisplay(), MainLayer() ) )
-
-"""
-    pyglet.resource.path.append('data')
-    pyglet.resource.reindex()
-    font.add_directory('data')
-
-    director.init( resizable=True, width=600, height=720 )
-    scene = Scene()
-    scene.add( MultiplexLayer(
-                    MainMenu(),
-                    OptionsMenu(),
-                    ScoresLayer(),
-                    ),
-                z=1 )
-    scene.add( BackgroundLayer(), z=0 )
-    director.run( scene )
-"""
